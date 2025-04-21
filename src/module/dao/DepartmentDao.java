512ca4716b60d7855138a77987167b0c68ee4f7d
Matheus Saraiva <matheus.saraiva.083@ufrn.edu.br>
2025-04-21 15:03:27 -0300

    Department and Seller interfaces

---
+++ src/module/dao/DepartmentDao.java
@@ -0,0 +1,15 @@
+package module.dao;
+
+import java.util.List;
+
+import module.entities.Department;
+
+public interface DepartmentDao {
+
+	void insert(Department obj);
+	void update(Department obj);
+	void deleteById(Integer id);
+	Department findById(Integer id);
+	List<Department> findAll();
+	
+}
