758f9ac186767452e15f5fd6af3f09e60268cfa7
Matheus Saraiva <matheus.saraiva.083@ufrn.edu.br>
2025-04-29 19:09:40 -0300

    findById

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import module.dao.DaoFactory;
+import module.dao.SellerDao;
 import module.entities.Department;
 import module.entities.Seller;
 
@@ -11,10 +13,9 @@ public class Main {
 
 			Department obj = new Department (1, "Books");
 			
-			System.out.println(obj);
 			
-			Seller seller = new Seller(21, "Matheus", "matheus@gmail.com", new Date(), 3000.0, obj);
-			
+			SellerDao sellerDao = DaoFactory.createSellerDao();
+			Seller seller = sellerDao.findById(3);
 			System.out.println(seller);
 	}
 
--- src/module/dao/DaoFactory.java
+++ src/module/dao/DaoFactory.java
@@ -1,5 +1,11 @@
 package module.dao;
 
+import db.DB;
+import module.dao.impl.SellerDaoJDBC;
+
 public class DaoFactory {
 
+	public static SellerDao createSellerDao() {
+		return new SellerDaoJDBC(DB.getConnection()); 
+	}
 }
